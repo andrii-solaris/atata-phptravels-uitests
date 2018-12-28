0c6578182b435cacef66fb7f17410ae8b176b45f
Andrii <stellaralnilam@gmail.com>
2018-12-28 12:53:22 +0000

    Added Hotel_Room_Add test

---
+++ src/PhpTravels.UITests.Components/Hotels/RoomsEditPage.cs
@@ -0,0 +1,29 @@
+﻿using Atata;
+using System;
+
+namespace PhpTravels.UITests.Components
+{
+    using _ = RoomsEditPage;
+
+    public class RoomsEditPage : Page<_>
+    {
+      [FindByXPath("//span[text()='Room Type']/..")]
+      public Link<_> RoomTypeDropdown { get; private set; }
+
+      [FindByIndex(2)]
+      [PressEnter]
+      public TextInput<_> RoomTypeInput { get; private set; }
+
+      [FindByXPath("//label[text()='Hotel']/..//a")]
+      public Link<_> HotelNameDropdown { get; private set; }
+
+      [FindByIndex(4)]
+      [PressEnter]
+      public TextInput<_> HotelNameInput { get; private set; }
+
+      [FindByName("basicprice")] 
+      public NumberInput<_> PriceInput { get; private set; }
+
+      public ButtonDelegate<RoomsPage, _> Submit { get; private set; }
+    }        
+}
--- src/PhpTravels.UITests/HotelTests.cs
+++ src/PhpTravels.UITests/HotelTests.cs
@@ -9,7 +9,8 @@ namespace PhpTravels.UITests
     public class HotelTests : UITestFixture
     {       
 
-        [Test]        
+        [Test]
+        [Order(1)]
         public void Hotel_Add([Random(1, 5, 1)] int stars)
         {
 
@@ -31,6 +32,7 @@ namespace PhpTravels.UITests
         }
 
         [Test]
+        [Order(2)]
         public void Hotel_Edit([Random(1, 5, 1)] int stars)
         {
 
@@ -52,7 +54,39 @@ namespace PhpTravels.UITests
                     EditButton.ClickAndGo().
                     Location.Set("Washington").
                     Submit().
-                    Hotels.Rows[x => x.Name == name].Content.Should.Contain("Washington");        
+                    Hotels.Rows[x => x.Name == name].Location.Should.Contain("Washington");        
+        }
+
+        [Test]
+        [Order(3)]
+        public void Hotel_Room_Add([Random(1, 5, 1)] int stars)
+        {
+
+            var startDate = HotelEditPage.RandomDate();
+
+            LoginAsAdmin();
+
+            Go.To<HotelsPage>().
+                Add.ClickAndGo().
+                    HotelName.SetRandom(out string name).
+                    HotelDescription.SetRandom(out string description).
+                    Hotelstars.Set(stars).
+                    Hoteltype.Set("Hotel").
+                    Location.Set("London").
+                    FromField.Set(startDate.ToString("dd/MM/yyyy")).
+                    ToField.Set(startDate.AddDays(365).ToString("dd/MM/yyyy")).
+                    Submit().
+                Hotels.Rows[x => x.Name == name].Should.BeVisible();
+
+            Go.To<RoomsPage>().
+                    Add.ClickAndGo().
+                    RoomTypeDropdown.Click().
+                    RoomTypeInput.Set("Triple Rooms").
+                    HotelNameDropdown.Click().
+                    HotelNameInput.Set(name).                    
+                    PriceInput.SetRandom(out int price).
+                    Submit().
+                    Rooms.Rows[x => x.Hotel == name].Price.Should.Contain(price.ToString());                    
         }
     }
 }
