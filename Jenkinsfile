cc90c5d2fc520ce0d80b71831c5df935fd937ee5
Andrii <stellaralnilam@gmail.com>
2019-01-16 23:30:36 +0000

    Pushed Jenkins files

---
+++ Jenkinsfile
@@ -0,0 +1,84 @@
+properties([
+    parameters([
+        string (name: 'branchName', defaultValue: 'master', description: 'Branch to get the tests from')
+    ])
+])
+
+def isFailed = false
+def branch = params.branchName
+def buildArtifactsFolder = "C:/BuildPackagesFromPipeline/$BUILD_ID"
+currentBuild.description = "Branch: $branch"
+
+def RunNUnitTests(String pathToDll, String condition, String reportName)
+{
+    try
+    {
+        bat "C:/Dev/NUnit.Console-3.9.0/nunit3-console.exe $pathToDll $condition --result=$reportName"
+    }
+    finally
+    {
+        stash name: reportName, includes: reportName
+    }
+}
+
+node('master') 
+{
+    stage('Checkout')
+    {
+        git branch: branch, url: 'https://github.com/andrii-solaris/atata-phptravels-uitests.git'
+    }
+    
+    stage('Restore NuGet')
+    {
+        powershell ".\\build.ps1 RestoreNuGetPackages"
+    }
+
+    stage('Build Solution')
+    {
+        powershell ".\\build.ps1 BuildSolution"
+    }
+
+    stage('Copy Artifacts')
+    {
+        powershell ".\\build.ps1 CopyArtifacts -BuildArtifactsFolder $buildArtifactsFolder"
+    }
+}
+
+catchError
+{
+    isFailed = true
+    stage('Run Tests')
+    {
+        parallel FirstTest: {
+            node('master') {
+                RunNUnitTests("$buildArtifactsFolder/AStepaniuk.Homework.dll", "--where cat==FirstTest", "TestResult1.xml")
+            }
+        }, SecondTest: {
+            node('Slave') {
+                RunNUnitTests("$buildArtifactsFolder/AStepaniuk.Homework.dll", "--where cat==SecondTest", "TestResult2.xml")
+            }
+        }
+    }
+    isFailed = false
+}
+
+node('master')
+{
+    stage('Reporting')
+    {
+        unstash "TestResult1.xml"
+        unstash "TestResult2.xml"
+
+        archiveArtifacts '*.xml'
+        nunit testResultsPattern: 'TestResult1.xml, TestResult2.xml'
+
+        if(isFailed)
+        {
+            slackSend color: 'danger', message: 'Tests failed.'
+        }
+        else
+        {
+            slackSend color: 'good', message: 'Tests passed.'
+        }
+    }
+}
---
+++ build.ps1
@@ -0,0 +1,117 @@
+#Requires -Version 5.0
+
+param
+(
+    [Parameter()]
+    [String[]] $TaskList = @("RestorePackages", "Build", "CopyArtifacts"),
+    
+    # Also add following parameters: 
+    #   Configuration
+    #   Platform
+    #   OutputPath
+    # And use these parameters inside BuildSolution function while calling for MSBuild.
+    # Use /p swith to pass the parameter. For example:
+    #   MSBuild.exe src/Solution.sln /p:Configuration=$Configuration
+    # More info here: https://docs.microsoft.com/en-us/visualstudio/msbuild/common-msbuild-project-properties?view=vs-2017
+
+    [Parameter()]
+    [String] $BuildArtifactsFolder,
+
+    [Parameter()]
+    [String] $Configuration = "Debug",
+
+    [Parameter()]
+    [String] $Platform = "Any CPU",
+
+    [Parameter()]
+    [String] $OutputPath ="src\PhpTravels.UITests\bin\Debug"
+)
+
+$NugetUrl = "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe"
+$NugetExe = Join-Path $PSScriptRoot "nuget.exe"
+$MSBuildExe = "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe"
+$Solution = "src/PhpTravels.UITests.sln"
+$NUnitConsoleExe = "C:\Program Files (x86)\NUnit.org\nunit-console\nunit3-console.exe"
+# Define additional variables here (MSBuild path, etc.)
+
+Function DownloadNuGet()
+{
+    if (-Not (Test-Path $NugetExe)) 
+    {
+        Write-Output "Installing NuGet from $NugetUrl..."
+        Invoke-WebRequest $NugetUrl -OutFile $NugetExe -ErrorAction Stop
+    }
+}
+
+Function RestoreNuGetPackages()
+{
+    DownloadNuGet
+    Write-Output 'Restoring NuGet packages...'
+
+    # NuGet.exe call here
+    & $NugetExe restore $Solution
+
+    if($LASTEXITCODE -ne 0){
+        Throw "An error occured while restoring nuget packages."
+    }
+}
+
+Function BuildSolution()
+{
+    Write-Output "Building '$Solution' solution..."
+
+    # MSBuild.exe call here
+    & $MSBuildExe $Solution /p:Configuration=$Configuration /p:Platform=$Platform
+     if($LASTEXITCODE -ne 0){
+        Throw "An error occured while building solution."
+    }
+}
+
+Function CopyBuildArtifacts()
+{
+    param
+    (
+        [Parameter(Mandatory)]
+        [String] $SourceFolder,
+        [Parameter(Mandatory)]
+        [String] $DestinationFolder
+    )
+
+    # Copy all files from $SourceFolder to $DestinationFolder
+    #
+    # Useful commands:
+    #   Test-Path - check if path exists
+    #   Remove-Item - remove folders/files
+    #   New-Item - create folder/file
+    #   Get-ChildItem - gets items from specified path
+    #   Copy-Item - copies item into destination folder
+    #
+    #     NOTE: you can chain methods using pipe (|) symbol. For example:
+    #           Get-ChildItem ... | Copy-Item ...
+    #
+    #           which will get items (Get-ChildItem) and will copy them (Copy-Item) to the target folder
+   
+    if (Test-Path -Path $DestinationFolder)
+    {
+        Remove-Item -Path $DestinationFolder -Recurse
+    }
+
+    New-Item -Path $DestinationFolder -ItemType "directory"
+
+    Copy-item -Force -Recurse -Verbose $SourceFolder -Destination $DestinationFolder
+}
+
+foreach ($Task in $TaskList) {
+    if ($Task.ToLower() -eq 'restorepackages')
+    {
+        RestoreNuGetPackages
+    }
+    if ($Task.ToLower() -eq 'build')
+    {
+        BuildSolution
+    }
+    if ($Task.ToLower() -eq 'copyartifacts')
+    {
+        CopyBuildArtifacts "$OutputPath" "$BuildArtifactsFolder"
+    }
+}
